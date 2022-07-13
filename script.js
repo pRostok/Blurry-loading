e483d8706669bee7a862f4af1de62c3583b40d25
pRostok <rostmart29@gmail.com>
2022-07-13 11:05:36 -0700

    added sript

---
+++ script.js
@@ -0,0 +1,22 @@
+const text = document.querySelector('.loading')
+const ba = document.querySelector('.back')
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
+
+
+let load = 0
+
+let int = setInterval(blurr, 30)
+
+function blurr() {
+    load++
+
+    if (load > 99) {
+        clearInterval(int)
+    }
+
+    text.innerText = `${load}%`
+    text.style.opacity = scale(load, 0, 100, 1, 0)
+    ba.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
